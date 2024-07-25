ee17868c060e87610ba4fc5453e8b6c0b5774885
Alibek <alibek3.14@mail.ru>
2024-07-25 21:34:16 +0500

    Finished task, added some basic tests to project

--- solution.py
+++ solution.py
@@ -1,24 +1,44 @@
 # Test task for junior python rpa position
 import os
 
-path = 'C:\\Users\\alibe\\PycharmProjects\\RPA_test_proj\\TestTask'
 
+def categorize_files_by_type(folder_path: str, min_file_size=None) -> dict[str: list]:
+    """The function searches for all the files in given root directory &
+    returns dictionary with file extensions as keys and file locations as it's value"""
+    if not os.path.exists(folder_path):  # here we check if root folder exists
+        return f"The path '{folder_path}' does not exist."
+    elif not os.path.isdir(folder_path):  # here we check if root folder is directory
+        return f"The root path should point to the directory.\nGot '{folder_path}' instead."
 
-def categorize_files_by_type(folder_path: str) -> dict[str: list]:
     result_dict = {}
-    if os.path.isfile(folder_path):
-        file_name, extension = os.path.splitext(folder_path)
-        result_dict[extension] = file_name
-    else:
-        for root, dirs, files in os.walk(folder_path):
-            for file in files:
-                file_path = os.path.join(root, file)
-                _dir, extension = os.path.splitext(file_path)
-                if extension not in result_dict:
-                    result_dict[extension] = []
-                result_dict[extension].append(file_path)
+
+    # Using the os.walk() function to recursively check for files
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+
+            # Here we can filter files by size
+            if min_file_size is not None:
+                current_size = os.path.getsize(file_path)
+                if current_size < min_file_size:
+                    continue
+
+            _dir, extension = os.path.splitext(file_path)
+            if extension not in result_dict:
+                result_dict[extension] = []
+            result_dict[extension].append(file_path)
+
+    # Here we can change output to required format, simply by uncommenting following block
+    # (couldn't write accurate tests for this format at the moment)
+
+    # output_format = "{" + "\n"
+    # for key, value in result_dict.items():
+    #     output_format += f"  '{key}': {value},\n"
+    # output_format = output_format.rstrip(",\n") + "\n}"
+    # return output_format
+
     return result_dict
 
 
-result = categorize_files_by_type('C:\\Users\\alibe\\PycharmProjects\\RPA_test_proj\\TestTask\\README.txt')
+result = categorize_files_by_type()
 print(result)
--- tests.py
+++ tests.py
@@ -1,13 +1,56 @@
 import tempfile
-
 import pytest
 import os
 from solution import categorize_files_by_type
 
 
+@pytest.fixture(scope='module')
+def create_test_root_directory():
+    # This function creates temporary directory for tests.
+    temp_dir = tempfile.mkdtemp()
+
+    with open(os.path.join(temp_dir, 'file_1.txt'), 'w') as f:
+        f.write("File number 1")
+
+    with open(os.path.join(temp_dir, 'file_2.txt'), 'w') as f:
+        f.write("File number 2")
+
+    with open(os.path.join(temp_dir, 'file_1.pdf'), 'w') as f:
+        f.write("PDF_File number 1")
+
+    with open(os.path.join(temp_dir, 'file_1.jpg'), 'w') as f:
+        f.write("Image number 1")
+
+    inner_folder = os.path.join(temp_dir, 'inner_folder')
+    os.makedirs(inner_folder)
+    with open(os.path.join(inner_folder, 'file_2.pdf'), 'w') as f:
+        f.write("PDF_File number 2")
+    return temp_dir
+
+
 def test_empty_dir():
-    assert '/empty_directory' == {}
+    temp_dir = tempfile.mkdtemp()
+    result = categorize_files_by_type(temp_dir)
+    assert result == {}
 
 
-def test_root_file():
-    pass
+def test_base_correctness(create_test_root_directory):
+    temp_dir = create_test_root_directory
+
+    result = categorize_files_by_type(temp_dir)
+
+    expect = {
+        '.txt': [
+            os.path.join(temp_dir, 'file_1.txt'),
+            os.path.join(temp_dir, 'file_2.txt')
+        ],
+        '.pdf': [
+            os.path.join(temp_dir, 'file_1.pdf'),
+            os.path.join(temp_dir, 'inner_folder','file_2.pdf')
+        ],
+        '.jpg': [
+            os.path.join(temp_dir, 'file_1.jpg')
+        ]
+    }
+
+    assert expect == result
